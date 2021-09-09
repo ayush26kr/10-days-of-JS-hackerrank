504c760ced58add23d3ef4a2a14dfa429a4ba7b8
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-09-10 03:25:26 +0530

    Create Remove From the end of an Array.js

---
+++ Arrays/Remove From the end of an Array.js
@@ -0,0 +1,13 @@
+var a = ['first', 'second', 'third'];
+console.log('Original Array:', a);
+
+// Remove the last element from the array
+let removed = a.pop();
+
+console.log('Modified Array:', a);
+console.log('Removed Element:', removed);
+
+
+// Original Array: [ 'first', 'second', 'third' ]
+// Modified Array: [ 'first', 'second' ]
+// Removed Element: third
