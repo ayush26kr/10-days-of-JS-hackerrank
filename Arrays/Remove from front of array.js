63c8d21494b0c3ef4e165112adf17a0ba737a59c
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-09-10 03:26:40 +0530

    Create Remove from front of array.js

---
+++ Arrays/Remove from front of array.js
@@ -0,0 +1,13 @@
+var a = ['first', 'second', 'third'];
+console.log('Original Array:', a);
+
+// Remove the first element from the array
+let removed = a.shift();
+
+console.log('Modified Array:', a);
+console.log('Removed Element:', removed);
+
+
+// Original Array: [ 'first', 'second', 'third' ]
+// Modified Array: [ 'second', 'third' ]
+// Removed Element: first
