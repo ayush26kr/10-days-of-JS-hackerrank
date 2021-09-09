ad98d79afab5713ac8aa93787c231e3dda7b189d
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-09-10 03:31:30 +0530

    Create Remove an Item by Index Position.js

---
+++ Arrays/Remove an Item by Index Position.js
@@ -0,0 +1,13 @@
+var a = ['first', 'second', 'third', 'fourth', 'fifth'];
+console.log('Original Array:', a);
+
+let position = 1;
+let elementsToRemove = 2;
+// Remove 'elementsToRemove' element(s) starting at 'position'
+a.splice(position, elementsToRemove);
+
+console.log('Modified Array:', a);
+
+// output
+// Original Array: [ 'first', 'second', 'third', 'fourth', 'fifth' ]
+// Modified Array: [ 'first', 'fourth', 'fifth' ]
